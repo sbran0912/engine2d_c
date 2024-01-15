cc3b3bd4e8c1d6cff0ddcf2f26da611a94900eaa
sbran0912 <95239081+sbran0912@users.noreply.github.com>
2024-01-15 21:01:14 +0100

    Add files via upload

--- engine2d_c/src/main.c
+++ engine2d_c/src/main.c
@@ -1,10 +1,12 @@
 #include "rl2d_core.h"
 
 int main() {
-    for (size_t i = 0; i < 20; i++)
-    {
-        printf("%i ", random(10, 40));
-    }
-    printf("\n\n");
+
+    Vector2 v1 = createVec((float)random(10,20), (float)random(100, 150));
+    Vector2 v2 = copyVec(v1);
+    v2.x = 99.0f;
+    printf("%f %f \n", v1.x, v1.y);
+    printf("%f %f \n", v2.x, v2.y);
+
     return EXIT_SUCCESS;
 }
--- engine2d_c/src/rl2d_core.h
+++ engine2d_c/src/rl2d_core.h
@@ -3,6 +3,41 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include "raylib.h"
+
+typedef struct Matrix3x3 {
+	float index[3][3];
+} Matrix3x3;
+
+typedef struct Matrix3x1 {
+	float index[3];
+} Matrix3x1;
+
+Vector2 createVec(float x, float y);
+Vector2 copyVec(Vector2 v);
+
+/*
+void set(float x, float y);
+void mult(float n);
+void div(float n);
+void add(Vec2d v);
+void sub(Vec2d v);
+float dot(Vec2d v);
+float cross(Vec2d v);
+void normalize();
+void limit(float max);
+float magsq();
+float mag();
+void setmag(float magnitude);
+float distance(Vec2d v);
+void rotate(Vec2d base, float n);
+Vector2 perp();
+Vector2 addVec(Vec2d v1, Vec2d v2);
+Vector2 subVec(Vec2d v1, Vec2d v2);
+Vector2 multVec(Vec2d v, float n);
+Vector2 divVec(Vec2d v, float n);
+void DrawArrow(Vec2d v_base, Vec2d v_target, Color c);
+*/
 
 int random(int range_start, int range_end);
 
